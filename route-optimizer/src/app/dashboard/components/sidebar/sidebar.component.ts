4fea8aeb9a660a16f143c6800cab441a7bb29dce
wojhan <w.haniewski@gmail.com>
2020-02-24 13:04:17 +0100

    Changed layout for company list

--- route-optimizer/src/app/dashboard/components/sidebar/sidebar.component.ts
+++ route-optimizer/src/app/dashboard/components/sidebar/sidebar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
 import { DashboardService } from '../../dashboard.service';
+import { IconDefinition, faTachometerAlt, faBuilding, faMoneyBillWave, faUsers } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-dashboard-sidebar',
@@ -7,6 +8,10 @@ import { DashboardService } from '../../dashboard.service';
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit {
+  faTachometerAlt: IconDefinition = faTachometerAlt;
+  faBuilding: IconDefinition = faBuilding;
+  faMoneyBillWave: IconDefinition = faMoneyBillWave;
+  faUsers: IconDefinition = faUsers;
   constructor(private dashboardService: DashboardService) {}
 
   @Input()
