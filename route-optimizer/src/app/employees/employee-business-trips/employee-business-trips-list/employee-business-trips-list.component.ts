6bfb1dc3645fd1a01c338da86dafb1d75352251a
wojhan <w.haniewski@gmail.com>
2020-02-26 14:48:56 +0100

    Employee business trips - layout

--- route-optimizer/src/app/employees/employee-business-trips/employee-business-trips-list/employee-business-trips-list.component.ts
+++ route-optimizer/src/app/employees/employee-business-trips/employee-business-trips-list/employee-business-trips-list.component.ts
@@ -3,11 +3,13 @@ import { IconDefinition, faEllipsisV } from '@fortawesome/free-solid-svg-icons';
 import { FormGroup, FormControl } from '@angular/forms';
 import { Observable, BehaviorSubject, merge } from 'rxjs';
 import { Page } from 'src/app/pagination';
-import { Employee } from '../../employees.service';
+import { Employee, EmployeesService } from '../../employees.service';
 import { environment } from 'src/environments/environment';
 import { BusinessTripsService, BusinessTrip } from 'src/app/business-trips/business-trips.service';
-import { debounceTime, startWith, switchMap, share } from 'rxjs/operators';
+import { debounceTime, startWith, switchMap, share, filter } from 'rxjs/operators';
 import { ActivatedRoute } from '@angular/router';
+import { DeleteModalComponent } from 'src/app/shared/components/delete-modal/delete-modal.component';
+import { MatDialog } from '@angular/material';
 
 @Component({
   selector: 'app-employee-business-trips-list',
@@ -21,11 +23,22 @@ export class EmployeeBusinessTripsListComponent implements OnInit {
   page: Observable<Page<BusinessTrip>>;
   pageUrl: BehaviorSubject<string>;
   employeeId: number;
+  employee: Employee;
 
-  constructor(private businessTripsService: BusinessTripsService, private route: ActivatedRoute) {}
+  constructor(
+    private businessTripsService: BusinessTripsService,
+    private employeesService: EmployeesService,
+    private route: ActivatedRoute,
+    private dialog: MatDialog
+  ) {}
 
   ngOnInit() {
     this.employeeId = +this.route.snapshot.paramMap.get('employeeId');
+
+    this.employeesService.getEmployee(this.employeeId).subscribe((employee: Employee) => {
+      this.employee = employee;
+    });
+
     this.pageUrl = new BehaviorSubject<string>(
       `${environment.apiUrl}api/employees/${this.employeeId}/business-trips/?format=json&page=1&page_size=40`
     );
@@ -40,6 +53,25 @@ export class EmployeeBusinessTripsListComponent implements OnInit {
     );
 
     this.page.subscribe(data => console.log(data));
+  }
+
+  deleteBusinessTrip(businessTrip: BusinessTrip) {
+    const dialogRef = this.dialog.open(DeleteModalComponent, {
+      width: '250px',
+      data: {
+        content: `Czy na pewno chcesz usunąć delegację ${businessTrip.startDate} - ${businessTrip.finishDate}?`,
+        ok: true
+      }
+    });
+
+    const dialogClosed: Observable<number> = dialogRef.afterClosed().pipe(filter(result => result !== undefined));
+    const deleteBusinessTrip = dialogClosed.pipe(switchMap(() => this.businessTripsService.deleteBusinessTrip(businessTrip.id)));
+
+    deleteBusinessTrip.subscribe({
+      next: () => {
+        this.onPageChanged(this.pageUrl.getValue());
+      }
+    });
   }
 
   onPageChanged(page: string): void {
